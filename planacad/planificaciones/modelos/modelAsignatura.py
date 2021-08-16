1e1416a732251caff0914b2c1f7824b38a1cbc35
Carlos Lopez Soto <carlitoslopezs@hotmail.com>
2021-08-16 20:51:46 -0300

    add foreign key of carrera in Asignatura and create a new table many to many (asignatura and carrera)

--- planacad/planificaciones/modelos/modelAsignatura.py
+++ planacad/planificaciones/modelos/modelAsignatura.py
@@ -1,5 +1,9 @@
+from planacad.planificaciones.modelos.modelCarrera import Carrera
+from planacad.planificaciones.modelos.modelProfesor import Profesor
 from django.db import models
+from planificaciones.modelos.modelCarrera import Carrera
 
 class Asignatura(models.Model): 
    id = models.AutoField(primary_key=True) 
    nombreMateria = models.CharField(max_length=50)
+   carrera = models.ForeignKey(Carrera, on_delete=models.CASCADE)
