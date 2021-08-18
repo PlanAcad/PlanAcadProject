680e8abe77768286712686c0a90be5c30407f3e3
nicmalegre <40902023+nicmalegre@users.noreply.github.com>
2021-08-18 20:41:52 -0300

    Register models for admin site and add __str__ method to models

--- planacad/planificaciones/modelos/modelProfesor.py
+++ planacad/planificaciones/modelos/modelProfesor.py
@@ -6,3 +6,6 @@ class Profesor(models.Model):
    apellido = models.CharField(max_length=50)
    situacion = models.CharField(max_length=50)
    dedicacion = models.CharField(max_length=50)
+
+   def __str__(self):
+        return f'{self.nombre} {self.apellido}'
