bcc3f69e4617f309c856962148dc9158f7a935ee
Carlos Lopez Soto <carlitoslopezs@hotmail.com>
2021-08-19 20:15:01 -0300

    fix name seccion3 and modified model planificacion

--- planacad/planificaciones/modelos/modelSeccion3.py
+++ planacad/planificaciones/modelos/modelSeccion3.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Profesor(models.Model): 
+class Seccion3(models.Model): 
    id = models.AutoField(primary_key=True) 
    Fundamentos = models.CharField(max_length=6000)
 
