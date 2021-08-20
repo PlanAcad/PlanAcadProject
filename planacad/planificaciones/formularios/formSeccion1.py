e882a7474b135ca31b8faca818d4435220b5862a
lucasmmaidana <lucasmaidanasoy@gmail.com>
2021-08-20 19:13:07 -0300

    Change Cursado field widget to radio buttons

--- planacad/planificaciones/formularios/formSeccion1.py
+++ planacad/planificaciones/formularios/formSeccion1.py
@@ -2,6 +2,12 @@ from django import forms
 from planificaciones.modelos.modelSeccion1 import Seccion1
 
 class Seccion1Form(forms.ModelForm):  
+    cursado = forms.ChoiceField(choices=[
+    ('A','Anual'),
+    ('1','1er Cuatrimestre'),
+    ('2','2do Cuatrimestre')
+    ], widget=forms.RadioSelect())
     class Meta:  
         model = Seccion1  
         fields = "__all__"
+        
