6bf8cc57f68198de3cea1c66733300bf8e8edbfb
Carlos Lopez Soto <carlitoslopezs@hotmail.com>
2021-09-07 17:40:11 -0300

    change model unidad and add migration

--- planacad/planificaciones/modelos/modelUnidad.py
+++ planacad/planificaciones/modelos/modelUnidad.py
@@ -8,6 +8,6 @@ from django.db import models
 class Unidad(models.Model): 
    id = models.AutoField(primary_key=True) 
    nombre_unidad = models.CharField(max_length=50)
-   planificiacion = models.ForeignKey(Planificacion, on_delete=models.CASCADE, null=True, blank=True)
+   planificacion = models.ForeignKey(Planificacion, on_delete=models.CASCADE, null=True, blank=True)
    def __str__(self):
         return "%s, %s" % (self.nombre_unidad,self.contenido)
