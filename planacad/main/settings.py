2494ce57aff5beebf17e0f97f724ecfe90340cd4
Carlos Lopez Soto <carlitoslopezs@hotmail.com>
2021-09-17 16:52:24 -0300

    fix url to deploy

--- planacad/main/settings.py
+++ planacad/main/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 'n%)qyk7n&3_uk+3zx&!z7ba1bykb1!9!0xhi$dw)n*v7kewn+9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['lucasmaidana.pythonanywhere.com']
-
+#ALLOWED_HOSTS = ['lucasmaidana.pythonanywhere.com']
+ALLOWED_HOSTS = []
 
 # Application definition
 
