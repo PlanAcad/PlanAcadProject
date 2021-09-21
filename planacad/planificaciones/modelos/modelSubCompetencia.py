913ff9d62332b2b7cec238cf032c350841c27bc2
lucasmmaidana <lucasmaidanasoy@gmail.com>
2021-09-21 19:57:51 -0300

    Fix campo descripcion

--- planacad/planificaciones/modelos/modelSubCompetencia.py
+++ planacad/planificaciones/modelos/modelSubCompetencia.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class SubCompetencia(models.Model): 
    id = models.AutoField(primary_key=True) 
-   descripcion = models.CharField(max_length=300)
+   descripcion = models.CharField(max_length=300, blank=True, null=True)
 
    def __str__(self):
         return "%s" % (self.capacidad)
