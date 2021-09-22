ca1d4d1a06693f8542feca7f45e00c49c051050c
lucasmmaidana <lucasmaidanasoy@gmail.com>
2021-09-21 22:03:10 -0300

    Update y eliminar subcompetencia

--- planacad/planificaciones/funcionesDeVistas/viewSubCompetencia.py
+++ planacad/planificaciones/funcionesDeVistas/viewSubCompetencia.py
@@ -1,6 +1,9 @@
 # Para usar los objetos y/o funciones de 'redirect'
 from planificaciones.modelos.modelCompetencia import Competencia
 from django.shortcuts import render, redirect  
+from django.http import  HttpResponseRedirect
+from django.urls import reverse
+
 ## import model and form
 from planificaciones.formularios.formSubCompetencia import SubCompetenciaForm
 from planificaciones.modelos.modelSubCompetencia import SubCompetencia 
@@ -28,11 +31,12 @@ def SubCompetenciaNew(request,id_planificacion,id_competencia):
     return render(request,'secciones/subcompetencias/index.html',{'planificacion':planificacion,'competencia':competencia,'form':form, 'mensaje_error': mensaje_error,'mensaje_exito':mensaje_exito}) 
   
   
-def SubCompetenciaUpdate(request, id_planificacion, id_competencia):  
+def SubCompetenciaUpdate(request, id_planificacion, id_competencia, id_subcompetencia):  
     mensaje_exito = None
     mensaje_error = None
     planificacion = Planificacion.objects.get(id=id_planificacion)
-    data = Competencia.objects.get(id=id_competencia)
+    competencia = Competencia.objects.get(id=id_competencia)
+    data = SubCompetencia.objects.get(id=id_subcompetencia)
     if request.method == "POST":  
         form = SubCompetenciaForm(request.POST, instance=data)  
         if form.is_valid():  
@@ -42,29 +46,29 @@ def SubCompetenciaUpdate(request, id_planificacion, id_competencia):
                 #Guardo
                 instance.save()
                 mensaje_exito="Guardamos los cambios correctamente."
-                #return HttpResponseRedirect(reverse('planificaciones:detallesprofesorcatedra', args=[id_planificacion]))
-                return redirect('planificaciones:competencias', id_planificacion=id_planificacion)
-                 
+                return HttpResponseRedirect(reverse('planificaciones:competenciaUpdate', args=[id_planificacion, id_competencia]))
+                
             except:  
                  mensaje_error = "No pudimos guardar los cambios."    
     else:  
         form = SubCompetenciaForm(instance=data)  
-    return render(request,'secciones/competencias/editar.html',{'data':data,'planificacion':planificacion,'form':form, 'mensaje_error': mensaje_error,'mensaje_exito':mensaje_exito}) 
+    return render(request,'secciones/subcompetencias/editar.html',{'data':data,'competencia':competencia,'planificacion':planificacion,'form':form, 'mensaje_error': mensaje_error,'mensaje_exito':mensaje_exito}) 
   
 
-def SubCompetenciaDestroy(request, id_planificacion, id_competencia):
+def SubCompetenciaDestroy(request, id_planificacion, id_competencia, id_subcompetencia):
     mensaje_exito = None
     mensaje_error = None
     if request.method == "POST":
         try:
-            competencia = Competencia.objects.get(id=id_competencia)  
-            competencia.delete()
+            subcompetencia = SubCompetencia.objects.get(id=id_subcompetencia)  
+            subcompetencia.delete()
             mensaje_exito = "Se ha borrado correctamente."        
         except:
             mensaje_error = "No pudimos borrar correctamente"  
         
-        return redirect('planificaciones:competencias', id_planificacion=id_planificacion)
-  
+        return HttpResponseRedirect(reverse('planificaciones:competenciaUpdate', args=[id_planificacion, id_competencia]))
+
+        
 def SubCompetenciaDetailView(request, id):
     mensaje_error = None
     subcompetencia = None
