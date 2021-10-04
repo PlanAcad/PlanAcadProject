620f02631be505ac2133d6306a4b0c63846ee300
lucasmmaidana <lucasmaidanasoy@gmail.com>
2021-10-04 20:37:07 -0300

    Fix issue

--- planacad/planificaciones/formularios/formDetalleProfesorCatedra.py
+++ planacad/planificaciones/formularios/formDetalleProfesorCatedra.py
@@ -1,12 +1,21 @@
 from django import forms  
+from django.utils.translation import gettext_lazy as _
 from planificaciones.modelos.modelDetalleProfesorCatedra import DetalleProfesorCatedra
 
 class DetalleProfesorCatedraForm(forms.ModelForm): 
 
     actividades = forms.CharField(widget=forms.Textarea()) 
     class Meta:  
-        model = DetalleProfesorCatedra  
-        exclude = ['planificacion']
+        model = DetalleProfesorCatedra         
+        fields = ['profesor', 'categoria', 'situacion', 'dedicacion', 'tareas']
+        exclude = ['actividades', 'planificacion']
         widgets = {
             'tareas': forms.CheckboxSelectMultiple()
+        }
+        labels = {
+            'profesor': _('Profesor'),
+            'categoria': _('Categoría'),
+            'situacion': _('Situación de Revista y/o Condición'),
+            'dedicacion': _('Dedicación'),
+            'tareas': _('Tareas/Funciones a realizar'),
         }
