4292ffdd5e38857be84246d147dbfbbcf6850adb
lucasmmaidana <lucasmaidanasoy@gmail.com>
2021-10-18 19:44:51 -0300

    Volverr a poner campos nombre y apellido

--- planacad/planificaciones/formularios/formDetalleProfesorCatedra.py
+++ planacad/planificaciones/formularios/formDetalleProfesorCatedra.py
@@ -3,9 +3,11 @@ from django.utils.translation import gettext_lazy as _
 from planificaciones.modelos.modelDetalleProfesorCatedra import DetalleProfesorCatedra
 
 class DetalleProfesorCatedraForm(forms.ModelForm): 
+    nombre_profesor = forms.CharField(required=False) 
+    apellido_profesor = forms.CharField(required=False) 
     class Meta:  
         model = DetalleProfesorCatedra         
-        fields = ['profesor', 'categoria', 'situacion', 'dedicacion', 'tareas']
+        fields = ['nombre_profesor', 'apellido_profesor', 'profesor', 'categoria', 'situacion', 'dedicacion', 'tareas']
         exclude = ['actividades', 'planificacion']
         widgets = {
             'tareas': forms.CheckboxSelectMultiple()
