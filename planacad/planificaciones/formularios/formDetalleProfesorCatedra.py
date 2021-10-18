6fe398bc0aa0817559cd195d46f61dfd27f8793a
lucasmmaidana <lucasmaidanasoy@gmail.com>
2021-10-18 19:46:28 -0300

    Volver a mostrar actividades

--- planacad/planificaciones/formularios/formDetalleProfesorCatedra.py
+++ planacad/planificaciones/formularios/formDetalleProfesorCatedra.py
@@ -3,12 +3,13 @@ from django.utils.translation import gettext_lazy as _
 from planificaciones.modelos.modelDetalleProfesorCatedra import DetalleProfesorCatedra
 
 class DetalleProfesorCatedraForm(forms.ModelForm): 
+    actividades = forms.CharField(widget=forms.Textarea())
     nombre_profesor = forms.CharField(required=False) 
     apellido_profesor = forms.CharField(required=False) 
     class Meta:  
         model = DetalleProfesorCatedra         
         fields = ['nombre_profesor', 'apellido_profesor', 'profesor', 'categoria', 'situacion', 'dedicacion', 'tareas']
-        exclude = ['actividades', 'planificacion']
+        exclude = ['planificacion']
         widgets = {
             'tareas': forms.CheckboxSelectMultiple()
         }
