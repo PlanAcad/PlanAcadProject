915beb21a64bf4571093136eb84bef69304c715d
lucasmmaidana <lucasmaidanasoy@gmail.com>
2021-11-06 20:01:04 -0300

    Fix tablas

--- planacad/planificaciones/formularios/formClase.py
+++ planacad/planificaciones/formularios/formClase.py
@@ -22,14 +22,21 @@ MONTHS = {
     12: ('12')
 }
 
+TIPO_EXAMEN = [
+      ('P', 'Examen parcial'),
+      ('R', 'Examen recuperatorio'),
+      ('F', 'Examen final'),
+      ('NA', 'No aplica')
+]
+
 class ClaseForm(forms.ModelForm):  
     lugar_desarrollo_de_clase = forms.MultipleChoiceField(
         required=False,
         widget=forms.CheckboxSelectMultiple,
         choices=LUGARES,
     )
-    fecha_clase = forms.DateField(widget=forms.SelectDateWidget(months=MONTHS))
-    es_examen = forms.CharField(widget=forms.CheckboxInput())
+    fecha_clase = forms.DateField(widget=forms.SelectDateWidget(months=MONTHS, attrs={'class': 'form-fecha'}))
+    es_examen = forms.CharField(widget=forms.RadioSelect(choices=TIPO_EXAMEN))
     class Meta:  
         model = Clase
         fields = ['profesor_a_cargo','lugar_desarrollo_de_clase', 'fecha_clase', 'es_examen', 'unidad_tematica_o_tema', 'cantidad_tareas','resultado_de_aprendizaje']
@@ -37,7 +44,7 @@ class ClaseForm(forms.ModelForm):
             'profesor_a_cargo': 'Profesor a cargo de la clase',
             'lugar_desarrollo_de_clase': 'Lugar de desarrollo de la clase',
             'fecha_clase': 'Fecha de la clase',
-            'es_examen': '',
+            'es_examen': ' ',
             'unidad_tematica_o_tema': 'Unidad temática / tema',
             'cantidad_tareas': 'Cantidad de tareas por clase / semana',
             'resultado_de_aprendizaje': 'Resultado de aprendizaje',
