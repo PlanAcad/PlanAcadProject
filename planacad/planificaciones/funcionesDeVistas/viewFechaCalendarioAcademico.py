b17da5010c80f62a72c8abd0aae12c74601eb461
Carlos Lopez Soto <carlitoslopezs@hotmail.com>
2021-12-15 20:09:23 -0300

    return calendario when it is not DN

--- planacad/planificaciones/funcionesDeVistas/viewFechaCalendarioAcademico.py
+++ planacad/planificaciones/funcionesDeVistas/viewFechaCalendarioAcademico.py
@@ -35,11 +35,11 @@ def CalendarioAcademicoIndex(request, año):
                 mensaje_exito="Se cargo el calendario correctamente"
             else:
                 mensaje_error="ya hay un calendario con esa fecha"
-            calendario =FechaCalendarioAcademico.objects.filter(fecha__year = año)                 
+            calendario =FechaCalendarioAcademico.objects.filter(fecha__year = año).exclude(actividad='DN').order_by('fecha')                 
         except:  
             mensaje_error="no se cargo nada de nada"  
     else:  
-        calendario = FechaCalendarioAcademico.objects.filter(fecha__year = año) 
+        calendario = FechaCalendarioAcademico.objects.filter(fecha__year = año).exclude(actividad='DN').order_by('fecha')  
     return render(request,'calendario/calendario-academico.html',{'calendario':calendario,'año':año,'mensaje_error': mensaje_error,
     'mensaje_exito':mensaje_exito}) 
 
@@ -100,6 +100,6 @@ def CerrarCalendarioAcademico(request, año):
         except:  
             mensaje_error="no se cargo nada de nada"  
     else:  
-        calendario = FechaCalendarioAcademico.objects.filter(fecha__year = año) 
+        calendario = FechaCalendarioAcademico.objects.filter(fecha__year = año).exclude(actividad='DN').order_by('fecha')
     return render(request,'calendario/calendario-academico-cerrar.html',{'calendario':calendario,'año':año,'mensaje_error': mensaje_error,
 'mensaje_exito':mensaje_exito}) 
