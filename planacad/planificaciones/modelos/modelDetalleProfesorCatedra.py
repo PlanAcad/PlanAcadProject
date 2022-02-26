32eec5bb0a4f914e0b5bfaecee8af27c8b043bbf
Carlos Lopez Soto <carlitoslopezs@hotmail.com>
2022-02-26 17:20:50 -0300

    copiar planificacion

--- planacad/planificaciones/modelos/modelDetalleProfesorCatedra.py
+++ planacad/planificaciones/modelos/modelDetalleProfesorCatedra.py
@@ -7,13 +7,13 @@ from planificaciones.modelos.modelTareasFunciones import TareasFunciones
 from planificaciones.modelos.modelPlanificacion import Planificacion
 
 class DetalleProfesorCatedra(models.Model): 
+   planificacion = models.ForeignKey(Planificacion,on_delete=models.CASCADE, null=True, blank=True)
    id = models.AutoField(primary_key=True)
    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, null=True, blank=True) 
    situacion = models.ForeignKey(Situacion, on_delete=models.CASCADE, null=True, blank=True)
    dedicacion = models.ForeignKey(Dedicacion, on_delete=models.CASCADE, null=True, blank=True)  
    profesor = models.ForeignKey(Profesor, on_delete=models.CASCADE, null=True, blank=True)
    tareas = models.ManyToManyField(TareasFunciones, blank=True)
-   planificacion = models.ForeignKey(Planificacion,on_delete=models.CASCADE, null=True, blank=True)
    
    actividades = models.CharField(max_length=3000, null=True, blank=True)
     
