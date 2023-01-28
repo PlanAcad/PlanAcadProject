0bdc899da3ee3f83060844404a71f5fbadeee13c
Carlos Lopez Soto <carlitoslopezs@hotmail.com>
2023-01-27 21:09:23 -0300

    fix: select profesor on distribucion de tareas add prefesor

--- planacad/planificaciones/funcionesDeVistas/viewDistribucionDeTareas.py
+++ planacad/planificaciones/funcionesDeVistas/viewDistribucionDeTareas.py
@@ -39,18 +39,11 @@ def DistribucionDeTareas(request, id_planificacion):
         if(item.estado == "G"):
             existen_correcciones_pendientes = "Existen correcciones pendientes de resolver"
 
-    form = DetalleProfesorCatedraForm()
+    form = DetalleProfesorCatedraForm(asignatura_id = planificacion.asignatura.id)
     if request.method == 'POST':
-        form = DetalleProfesorCatedraForm(request.POST)
+        form = DetalleProfesorCatedraForm(request.POST,asignatura_id = planificacion.asignatura.id)
         if form.is_valid():
             try:  
-                nombre_profesor = form.cleaned_data["nombre_profesor"]
-                apellido_profesor = form.cleaned_data["apellido_profesor"]
-                profesor = User.objects.filter(first_name__iexact=nombre_profesor).filter(last_name__iexact=apellido_profesor).first()
-
-                if not profesor:
-                    profesor = User.objects.create(first_name=nombre_profesor, last_name=apellido_profesor)
-
                 distribucion_de_tareas = form.save(commit=False)
                 distribucion_de_tareas.planificacion_id = planificacion.id
                 distribucion_de_tareas.profesor = profesor
@@ -82,73 +75,7 @@ def DistribucionDeTareas(request, id_planificacion):
         "mensaje_error": mensaje_error,
     }
 
-    return render(request,"secciones/distribucion-de-tareas/index.html", context) 
-
-    planificacion = Planificacion.objects.get(id=id_planificacion) 
-    detalles_profesores_catedra = DetalleProfesorCatedra.objects.filter(planificacion = planificacion) 
-    distribucionTareasForm = DistribucionTareasForm(instance=planificacion)
-    profesores =  detalles_profesores_catedra.filter(categoria__categoria = "Titular") | detalles_profesores_catedra.filter(categoria__categoria = "Adjunto")
-    profesores_auxiliares =  detalles_profesores_catedra.exclude(categoria__categoria = "Titular").exclude(categoria__categoria = "Adjunto")
-
-    mensaje_exito = None
-    mensaje_error = None
-    #CORRECCIONES
-    correcciones = Correccion.objects.filter(Q(planificacion_id = id_planificacion) & Q(seccion = 12)).prefetch_related('comentarios')
-    existen_correcciones_pendientes = None
-    #Forms Correcciones y Comentarios
-    correccionForm = CorreccionForm()
-    comentarioForm = ComentarioForm()
-    
-    for item in correcciones:
-        print(item.estado)
-        if(item.estado == "G"):
-            existen_correcciones_pendientes = "Existen correcciones pendientes de resolver"
-
-    form = DetalleProfesorCatedraForm()
-    if request.method == 'POST':
-        form = DetalleProfesorCatedraForm(request.POST)
-        if form.is_valid():
-            try:  
-                nombre_profesor = form.cleaned_data["nombre_profesor"]
-                apellido_profesor = form.cleaned_data["apellido_profesor"]
-                profesor = User.objects.filter(first_name__iexact=nombre_profesor).filter(last_name__iexact=apellido_profesor).first()
-
-                if not profesor:
-                    profesor = User.objects.create(first_name=nombre_profesor, last_name=apellido_profesor)
-
-                distribucion_de_tareas = form.save(commit=False)
-                distribucion_de_tareas.planificacion_id = planificacion.id
-                distribucion_de_tareas.profesor = profesor
-                distribucion_de_tareas.save()
-                form.save_m2m()
-
-                    
-                mensaje_exito="Añadimos el docente correctamente."  
-                 
-            except:  
-                 mensaje_error = "No pudimos añadir el docente." 
-
-        else:
-            mensaje_error = "No pudimos añadir el docente." 
-
-    context = {
-        'planificacion': planificacion,
-        'profesores': profesores,
-        'profesores_auxiliares': profesores_auxiliares,
-        "form": form,
-        'distribucionTareasForm': distribucionTareasForm,
-        'correcciones':correcciones,
-        #Forms Correcciones
-        'correccion_form': correccionForm,
-        'comentario_form':comentarioForm,
-        #
-        'existen_correcciones_pendientes':existen_correcciones_pendientes,
-        "mensaje_exito": mensaje_exito,
-        "mensaje_error": mensaje_error,
-        "mensaje_guardo": guardo
-    }
-
-    return render(request,"secciones/distribucion-de-tareas/index.html", context)    
+    return render(request,"secciones/distribucion-de-tareas/index.html", context)   
 
 @login_required
 def UpdateDistribucionDeTareasPlanif(request, id_planificacion):  
@@ -174,12 +101,12 @@ def UpdateDistribucionDeTareasPlanif(request, id_planificacion):
 def UpdateDistribucionDeTareas(request, id_planificacion, id_detalleprofesorcatedra):  
     planificacion = Planificacion.objects.get(id=id_planificacion)
     data = DetalleProfesorCatedra.objects.get(id=id_detalleprofesorcatedra)
-    form = DetalleProfesorCatedraForm(instance=data)
+    form = DetalleProfesorCatedraForm(instance=data,asignatura_id = planificacion.asignatura.id)
     mensaje_exito = None
     mensaje_error = None
 
     if request.method == "POST":  
-        form = DetalleProfesorCatedraForm(request.POST, instance=data) 
+        form = DetalleProfesorCatedraForm(request.POST, instance=data,asignatura_id = planificacion.asignatura.id) 
         if form.is_valid():  
             try:  
                 instance = form.save(commit=False)
