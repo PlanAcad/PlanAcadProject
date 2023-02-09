a3c39a7dda5b487a4b0f697786f3e37882e3f471
Carlos Lopez Soto <carlitoslopezs@hotmail.com>
2023-02-09 20:39:51 -0300

    ci: update

--- planacad/main/settings/production.py
+++ planacad/main/settings/production.py
@@ -9,8 +9,6 @@ SECRET_KEY = 'n%)qyk7n&3_uk+3zx&!z7ba1bykb1!9!0xhi$dw)n*v7kewn+9'
 SCM_DO_BUILD_DURING_DEPLOYMENT=True
 WEBSITE_WEBDEPLOY_USE_SCM =True
 
-ENABLE_ORYX_BUILD = True
-
 DEBUG = False
 
 ALLOWED_HOSTS = ['https://planacad.azurewebsites.net']
