9cf27f2409056c88fa8ea6b21dcddb4e12ef542e
Carlos Lopez Soto <carlitoslopezs@hotmail.com>
2023-02-10 11:13:34 -0300

    ci: update

--- planacad/main/wsgi.py
+++ planacad/main/wsgi.py
@@ -9,11 +9,11 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
+# from dj_static import Cling
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings.production')
 
-# application = get_wsgi_application()
+application = get_wsgi_application()
 
 
-application = Cling(get_wsgi_application())
+# application = Cling(get_wsgi_application())
