592211fa192b41b53a6cd1bd4981e45332f342ef
Carlos Lopez Soto <carlitoslopezs@hotmail.com>
2023-02-11 17:40:52 -0300

    fix: update dabase

--- planacad/planificaciones/modelos/modelContenido.py
+++ planacad/planificaciones/modelos/modelContenido.py
@@ -5,7 +5,7 @@ from planificaciones.modelos.modelUnidad import Unidad
 
 class Contenido(models.Model): 
    objetivos = models.CharField(max_length=3000)
-   contenido = models.CharField(max_length=5000)
+   contenido = models.CharField(max_length=3000)
    carga_horaria = models.CharField(max_length=100)
 
    # Relations
--- planacad/planificaciones/modelos/modelFundamentacion.py
+++ planacad/planificaciones/modelos/modelFundamentacion.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Fundamentacion(models.Model): 
    id = models.AutoField(primary_key=True) 
-   fundamentos = models.CharField(max_length=6000)
+   fundamentos = models.CharField(max_length=4000)
 
    def __str__(self):
         return str(self.id)
