29de50e70ffde32a23a4f170bac831390a74bf9d
Carlos Lopez Soto <carlitoslopezs@hotmail.com>
2023-02-15 16:11:51 -0300

    fix: queryset to set profesores in asignatura, now can asing profesores or jefe de carrera

--- planacad/planificaciones/formularios/formAsignatura.py
+++ planacad/planificaciones/formularios/formAsignatura.py
@@ -2,10 +2,16 @@ from django import forms
 from planificaciones.modelos.modelAsignatura import Asignatura
 from django.contrib.auth.models import User, Group
 from django.utils.translation import gettext_lazy as _
+from django.db.models import Q
+
+profesor_group = Group.objects.get(name='profesor')
+jefe_carrera_group = Group.objects.get(name='jefe de carrera')
+queryset = User.objects.filter(groups__in=[profesor_group, jefe_carrera_group])
+# queryset = queryset.filter(Q(groups=profesor_group) | Q(groups=jefe_carrera_group))
 
 class AsignaturaForm(forms.ModelForm):
     profesor = forms.ModelMultipleChoiceField(
-        queryset= User.objects.filter(groups= Group.objects.get(name='profesor')),
+        queryset= queryset,
         widget=forms.CheckboxSelectMultiple(attrs={'class': 'multiple-select-list'}))  
     class Meta:  
         model = Asignatura  
