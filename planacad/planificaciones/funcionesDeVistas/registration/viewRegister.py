09905104c0c9a475f8dc2d5014d510c31c74c348
Carlos Lopez Soto <carlitoslopezs@hotmail.com>
2023-02-25 11:46:31 -0300

    fix: problem with bulk register

--- planacad/planificaciones/funcionesDeVistas/registration/viewRegister.py
+++ planacad/planificaciones/funcionesDeVistas/registration/viewRegister.py
@@ -5,7 +5,10 @@ from planificaciones.modelos.modelCarrera import Carrera
 from django.contrib.auth.models import User
 from planificaciones.formularios.registration.formRegistration import CreateUserForm
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+import numpy as np
 
+@login_required
 def registerView(request):
     if(request.method == 'POST'):
         form = CreateUserForm(request.POST)
@@ -31,25 +34,28 @@ def registerView(request):
         }
         return render(request,'registration/register.html', context)
 
+@login_required
 def bulkRegister(request):
     if request.method == 'POST':
         # Obtener el rol del usuario desde el formulario
         role = request.POST.get('role')
-        print(role)
         # Leer el archivo Excel y convertirlo en un DataFrame
         df = pd.read_excel(request.FILES['excel_file'],engine='openpyxl')
+        df['legajo'] = pd.to_numeric(df['legajo'], errors='coerce').fillna(0).astype(np.int64)
+        df['dni'] = pd.to_numeric(df['dni'], errors='coerce').fillna(0).astype(np.int64)
         # Iterar sobre cada fila del DataFrame y crear usuarios de Django
         for _, row in df.iterrows():
-            user = User.objects.create_user(
-                username= row['first_name']+row['last_name'],
-                email=row['email'],
-                password=row['dni'],
-                first_name=row['first_name'],
-                last_name=row['last_name'],
-            )
-            group = Group.objects.get(name=role)
-            group.user_set.add(user)
-            carreras = Carrera.objects.get(nombre_carrera = row['carrera'])
-            user.carrera.add(carreras)
-            user.save()
+            if row['legajo']:
+                user = User.objects.create_user(
+                    username= row['legajo'],
+                    email=row['email'],
+                    password=row['dni'],
+                    first_name=row['first_name'],
+                    last_name=row['last_name']
+                )
+                group = Group.objects.get(name=role)
+                group.user_set.add(user)
+                carreras = Carrera.objects.get(nombre_carrera = row['carrera'])
+                user.carrera.add(carreras)
+                user.save()
         return redirect('planificaciones:usuarios')
