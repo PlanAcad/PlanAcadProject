80298f14d20bb888c7318b9c65266f6a4ece4d88
Carlos Lopez Soto <carlitoslopezs@hotmail.com>
2023-03-10 20:55:47 -0300

    show carrera and rol when update user

--- planacad/planificaciones/formularios/registration/formEditProfile.py
+++ planacad/planificaciones/formularios/registration/formEditProfile.py
@@ -2,7 +2,6 @@ from django.contrib.auth.forms import UserChangeForm
 from django.contrib.auth.models import User, Group
 from planificaciones.modelos.modelCarrera import Carrera
 from django.utils.translation import gettext_lazy as _
-from django.contrib.auth import password_validation
 from django import forms
 
 
@@ -10,17 +9,18 @@ class EditUserForm(UserChangeForm):
     first_name = forms.CharField(max_length=50, required=True)
     last_name = forms.CharField(max_length=50, required=True)
     email = forms.EmailField()
-    groups = forms.ModelChoiceField(queryset=Group.objects.all(), label="Rol")
+    rol = forms.ModelChoiceField(queryset=Group.objects.all())
     carrera = forms.ModelChoiceField(queryset=Carrera.objects.all())
 
     class Meta:
         model = User
         fields = [
-            'username', 'first_name', 'last_name', 'email', 'groups', 'carrera'
+            'username', 'first_name', 'last_name', 'email', 'rol', 'carrera'
         ]
+        exclude  = ['password']
         labels = {
             'username': _('Nombre de Usuario (Legajo)'),
             'first_name': _('Nombre/s'),
             'last_name': _('Apellido/s'),
-            'groups': _('Rol')
+            'rol': _('Rol')
         }
