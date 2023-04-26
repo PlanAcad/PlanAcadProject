d550661d8b97cea1a857ede7f37e3fef50d60bc6
Carlos Lopez Soto <carlitoslopezs@hotmail.com>
2023-04-26 18:59:01 -0300

    feat: add message in competencias y subcompetencias

--- planacad/planificaciones/funcionesDeVistas/viewSubCompetencia.py
+++ planacad/planificaciones/funcionesDeVistas/viewSubCompetencia.py
@@ -3,7 +3,9 @@ from planificaciones.modelos.modelCompetencia import Competencia
 from django.shortcuts import render, redirect  
 from django.http import  HttpResponseRedirect
 from django.urls import reverse
-
+from django.db.models import Q
+from django.contrib import messages
+from planificaciones.modelos.modelCorrecciones import Correccion
 ## import model and form
 from planificaciones.formularios.formSubCompetencia import SubCompetenciaForm
 from planificaciones.funcionesDeVistas import viewCorreccion
@@ -26,11 +28,15 @@ def SubCompetenciaNew(request,id_planificacion,id_competencia):
                 instance.competencia_id=competencia.id
                 #Guardo
                 instance.save()
+                messages.success(request, 'Se ha guardado con éxito')
                 return redirect('planificaciones:competenciaUpdate', id_planificacion=id_planificacion,id_competencia=id_competencia)
             except:  
                  mensaje_error = "No pudimos crear correctamente"    
+                 messages.error(request, 'La operación falló') 
     else:  
-        form = SubCompetenciaForm()  
+        form = SubCompetenciaForm()
+        #CORRECCIONES
+        correcciones = Correccion.objects.filter(Q(planificacion_id = id_planificacion) & Q(seccion = 5)).prefetch_related('comentarios')  
         correcciones = viewCorreccion.OrderCorrecciones(correcciones)
     correccionesEnSecciones = viewCorreccion.CorreccionesEnSecciones(id_planificacion)
     return render(request,'secciones/subcompetencias/index.html',{'planificacion':planificacion,'competencia':competencia,'form':form, 'correccionesEnSecciones':correccionesEnSecciones, 'mensaje_error': mensaje_error,'mensaje_exito':mensaje_exito}) 
@@ -50,13 +56,17 @@ def SubCompetenciaUpdate(request, id_planificacion, id_competencia, id_subcompet
                 instance.planificacion_id=planificacion.id
                 #Guardo
                 instance.save()
+                messages.success(request, 'Se ha guardado con éxito')
                 mensaje_exito="Guardamos los cambios correctamente."
                 return HttpResponseRedirect(reverse('planificaciones:competenciaUpdate', args=[id_planificacion, id_competencia]))
                 
             except:  
-                 mensaje_error = "No pudimos guardar los cambios."    
+                 mensaje_error = "No pudimos guardar los cambios."   
+                 messages.error(request, 'La operación falló')  
     else:  
-        form = SubCompetenciaForm(instance=data)  
+        form = SubCompetenciaForm(instance=data)
+        #CORRECCIONES
+        correcciones = Correccion.objects.filter(Q(planificacion_id = id_planificacion) & Q(seccion = 5)).prefetch_related('comentarios')  
         correcciones = viewCorreccion.OrderCorrecciones(correcciones)
     correccionesEnSecciones = viewCorreccion.CorreccionesEnSecciones(id_planificacion)
     return render(request,'secciones/subcompetencias/editar.html',{'data':data,'competencia':competencia,'planificacion':planificacion,'form':form, 'correccionesEnSecciones':correccionesEnSecciones, 'mensaje_error': mensaje_error,'mensaje_exito':mensaje_exito}) 
